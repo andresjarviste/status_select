4c566c2556908c9d1a0a76cc04c7b1574b4449ca
Andres Järviste <andres.jarviste@wbscodingschool.com>
2022-07-22 20:36:11 +0300

    build

---
+++ scoro_status/build/envparams.js
@@ -0,0 +1,6 @@
+"use strict";
+var _a;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    apiKey: (_a = process.env.API_KEY) !== null && _a !== void 0 ? _a : ''
+};
